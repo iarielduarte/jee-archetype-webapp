458e0064c59cc3c26dadfeec4e2bb5460eefad8e
iarielduarte <iariel.javauser@gmail.com>
2013-08-25 02:58:06 -0300

    new project spring MVC with hibernate

---
+++ springmvc-hib-withoutxml/src/main/java/com/sprhib/model/Team.java
@@ -0,0 +1,74 @@
+package com.sprhib.model;
+
+/**
+ * @author Ariel Duarte
+ */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="teams")
+public class Team {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="telephone")
+	private String telephone;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="project")
+	private String project;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+	
+
+}
